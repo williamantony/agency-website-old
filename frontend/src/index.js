3b7a839d3433de4cc07e6ade13cf65422692a227
William Antony <william@pathadan.com>
2018-03-13 23:08:42 -0500

    Implemented Redux, Redux-Thunk and React-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import Reducers from './redux/reducers';
+
+import App from './App';
+import './index.css';
+
+const ReduxStore = createStore(Reducers, applyMiddleware(ReduxThunk));
+
+ReactDOM.render(
+  <Provider store={ ReduxStore }>
+    <Router>
+      <Route path="/" component={ App } />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
