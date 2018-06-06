1d3dbbe574672bea8468a8f375d752c83ad95ff1
William Antony <william@pathadan.com>
2018-06-06 12:52:23 -0500

    Added main routes to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import Reducers from './redux/reducers';
 
-import App from './App';
 import './index.css';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import Landing from './pages/Landing/Landing';
+import Services from './pages/Services/Services';
+import Contact from './pages/Contact/Contact';
+import About from './pages/About/About';
+import Careers from './pages/Careers/Careers';
 
 const ReduxStore = createStore(Reducers, applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
   <Provider store={ ReduxStore }>
     <Router>
-      <Route path="/" component={ App } />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/careers" component={Careers} />
+        <Route component={ErrorPage} />
+      </Switch>
     </Router>
   </Provider>,
   document.getElementById('root')
