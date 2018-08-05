e3f0eaef9c8f459cd144b4e49697f59beb606dbb
William Antony <william@pathadan.com>
2018-08-04 23:12:40 -0500

    Modified Hamburger for light theme

--- frontend/src/components/Hamburger/Hamburger.js
+++ frontend/src/components/Hamburger/Hamburger.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setNavigationVisibility } from '../../redux/actions';
 import './Hamburger.css';
 
 class Hamburger extends Component {
@@ -6,8 +8,15 @@ class Hamburger extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      theme: 'light',
       opened: false,
     };
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({
+      theme: props.theme.style,
+    });
   }
 
   handleClick = (event) => {
@@ -15,12 +24,14 @@ class Hamburger extends Component {
     this.setState({
       opened: !this.state.opened,
     });
+    this.props.setNavigationVisibility(true);
   }
 
   render() {
     return (
       <div
         className="Hamburger"
+        data-theme={this.state.theme}
         onClick={this.handleClick}
       >
         <div
@@ -38,4 +49,14 @@ class Hamburger extends Component {
 
 }
 
-export default Hamburger;
+const mapStateToProps = (state) => {
+  return {
+    theme: state.theme,
+  };
+};
+
+const mapDispatchToProps = {
+  setNavigationVisibility,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Hamburger);
