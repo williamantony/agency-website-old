aca7b5ba7536c2c34a1da64c478e92495dd6c3ea
William Antony <william@pathadan.com>
2018-08-06 23:46:35 -0500

    Modified Landing Component

--- frontend/src/pages/Landing/Landing.js
+++ frontend/src/pages/Landing/Landing.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './Landing.css';
+import MainServices from '../../components/MainServices/MainServices';
 
 class Landing extends Component {
 
   render() {
     return (
       <div className="Landing">
+        <div className="Landing__bg">
+          <div className="Landing__bg__img Landing__bg__img--worldmap" />
+        </div>
+        <section>
+          <div className="Landing__Cover">
+            <div className="wrapper max-width--1500px">
+              <h1 className="homepage-heading">
+                Building Better Software,<br/>is our job.<br/>
+                <span className="h3__subheading">
+                  We take our jobs very seriously.
+                </span>
+              </h1>
+            </div>
+          </div>
+        </section>
+        
+        <section>
+          <div className="wrapper max-width--1500px">
+            <MainServices />
+          </div>
+        </section>
 
       </div>
     );
