6f2f9ad4b2d8d19c80efde3e413e27260f7ba32e
William Antony <william@pathadan.com>
2018-08-10 23:28:55 -0500

    Added ContactForm and ContactInfo to Contact page

--- frontend/src/pages/Contact/Contact.js
+++ frontend/src/pages/Contact/Contact.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import './Contact.css';
+import ContactInfo from '../../components/ContactInfo/ContactInfo';
+import ContactForm from '../../components/ContactForm/ContactForm';
 
 class Contact extends Component {
 
   render() {
     return (
       <div className="Contact">
+        <div className="Contact__cover">
+          <div className="Contact__cover__image" />
+          <div className="Contact__cover__dark-layer" />
+        </div>
 
+        <section className="Contact__page-info">
+          <div className="wrapper max-width--1500px">
+            <div className="Contact__page-info__content">
+              <h1 className="Contact__page-info__title">Contact</h1>
+            </div>
+          </div>
+        </section>
+
+        <section className="Contact__info">
+          <div className="wrapper max-width--1500px">
+            <ContactInfo />
+          </div>
+        </section>
+
+        <section className="Contact__form">
+          <div className="wrapper max-width--1500px">
+            <ContactForm />
+          </div>
+        </section>
       </div>
     );
   }
