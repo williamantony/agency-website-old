760aacd095b7fc8700931be1a0d83978a4ad9f79
William Antony <william@willsquad.com>
2018-10-22 22:43:14 -0500

    Created LandingCoverBackground Component

---
+++ frontend/src/components/LandingCover/components/LandingCoverBackground/LandingCoverBackground.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import './LandingCoverBackground.css';
+
+const LandingCoverBackground = () => (
+  <Fragment>
+    <div className="LandingCoverBackground__image" />
+    <div className="LandingCoverBackground__bg" />
+  </Fragment>
+);
+
+export default LandingCoverBackground;
