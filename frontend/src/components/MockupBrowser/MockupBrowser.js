f8322a2f104afef20b76404fe2e69246298f15c9
William Antony <william@willsquad.com>
2018-10-31 22:52:56 -0500

    Modified MockupBrowser & MockupPhone Components

--- frontend/src/components/MockupBrowser/MockupBrowser.js
+++ frontend/src/components/MockupBrowser/MockupBrowser.js
@@ -1,32 +1,29 @@
 import React, { Component } from 'react';
 import './MockupBrowser.css';
+import image from '../../static/snapshot/websnapshot.png';
 
 class MockupBrowser extends Component {
 
   render() {
     return (
-      <div className="MockupBrowserContainer">
-
-        <div className="MockupBrowser">
-          <div className="MockupBrowser__header">
-            <div className="MockupBrowser__header__actions">
-              <div className="MockupBrowser__header__actions__buttons" />
-              <div className="MockupBrowser__header__actions__buttons" />
-              <div className="MockupBrowser__header__actions__buttons" />
-            </div>
-            <div className="MockupBrowser__header__addressbar">
-              <div className="MockupBrowser__header__addressbar__text">
-                <span className="protocol">https://</span>
-                www.willsquad.com/
-              </div>
-              <div className="MockupBrowser__header__reload" />
-            </div>
+      <div className="MockupBrowser">
+        <div className="MockupBrowser__header">
+          <div className="MockupBrowser__header__actions">
+            <div className="MockupBrowser__header__actions__buttons" />
+            <div className="MockupBrowser__header__actions__buttons" />
+            <div className="MockupBrowser__header__actions__buttons" />
           </div>
-          <div className="MockupBrowser__body">
-          
+          <div className="MockupBrowser__header__addressbar">
+            <div className="MockupBrowser__header__addressbar__text">
+              <span className="protocol">https://</span>
+              www.willsquad.com/
+            </div>
+            <div className="MockupBrowser__header__reload" />
           </div>
         </div>
-
+        <div className="MockupBrowser__body">
+          <img src={image} />
+        </div>
       </div>
     );
   }
