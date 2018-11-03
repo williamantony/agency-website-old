6c8367047b1c2bd7c816d519c8815ecc1158d397
William Antony <william@willsquad.com>
2018-11-02 23:23:29 -0500

    Modified Pricing Component

--- frontend/src/components/Pricing/Pricing.js
+++ frontend/src/components/Pricing/Pricing.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Pricing.css';
-import PricingAdvantages from './components/PricingAdvantages/PricingAdvantages';
+import PricingTypes from './components/PricingTypes/PricingTypes';
+import SectionIcon from '../SectionIcon/SectionIcon';
+import SectionTitle from '../SectionTitle/SectionTitle';
+import SectionDescription from '../SectionDescription/SectionDescription';
+import SectionSummary from '../SectionSummary/SectionSummary';
 
-class Pricing extends Component {
-
-  render() {
-    return (
-      <div className="Pricing">
-        <div className="Pricing__icon" />
-        <div className="Pricing__title">In-budget Pricing</div>
-        <div className="Pricing__subtitle">
+const Pricing = () => {
+  return (
+    <div className="Pricing">
+      <SectionSummary align="center">
+        <SectionIcon icon="simple-pricing" />
+        <SectionTitle title="Simple Pricing" />
+        <SectionDescription>
           Browser compatability test is one of the most important step in our development process.
-          This ensures that your website will work on all the popular browsers in the same way as expected.  
-        </div>
-        <PricingAdvantages />
-      </div>
-    );
-  }
-
+          This ensures that your website will work on all the popular browsers in the same way as expected.
+        </SectionDescription>
+      </SectionSummary>
+      <PricingTypes />
+    </div>
+  );
 }
 
 export default Pricing;
