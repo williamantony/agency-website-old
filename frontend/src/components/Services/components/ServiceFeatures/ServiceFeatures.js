09094d41544ba0c289113e10d7f2359eb034f337
William Antony <william@willsquad.com>
2018-11-06 22:51:14 -0600

    Created ServiceFeatures Component

---
+++ frontend/src/components/Services/components/ServiceFeatures/ServiceFeatures.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ServiceFeatures.css';
+
+const ServiceFeatures = (props) => {
+  return (
+    <div className="ServiceFeatures">
+      <div className="ServiceFeatures__image" />
+      <div className="ServiceFeatures__content">
+        { 
+          props.features.map((feature, index) => (
+            <div className="ServiceFeatures__feature" key={index}>
+              <div className="ServiceFeatures__feature__icon" />
+              <div className="ServiceFeatures__feature__title">{ feature.title }</div>
+              <div className="ServiceFeatures__feature__description">{ feature.description }</div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default ServiceFeatures;
