3ba718570733e326c1f97f2ab305f2736dc58642
William Antony <william@willsquad.com>
2018-12-09 23:18:51 -0600

    Created InputRow Component

---
+++ frontend/src/components/reusable/InputRow/InputRow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './InputRow.css';
+
+const InputRow = (props) => (
+  <div className="InputRow">
+    { props.children }
+  </div>
+);
+
+export default InputRow;
