408fa4c5a07612dcf633fbdafffe6f2a8b132a60
William Antony <william@willsquad.com>
2018-12-13 22:16:56 -0600

    Created LandingServicesSection Component

---
+++ frontend/src/components/fixed/LandingServices/component/LandingServicesSection/LandingServicesSection.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './LandingServicesSection.css';
+import LandingServicesSectionSummary from '../LandingServicesSectionSummary/LandingServicesSectionSummary';
+import LandingServicesItem from '../LandingServicesItem/LandingServicesItem';
+
+const LandingServicesSection = (props) => {
+  const {
+    align,
+    summary,
+    services,
+  } = props;
+
+  return (
+    <div className={`LandingServicesSection ${align === 'right' ? `LandingServicesSection--right` : ``}`}>
+      <div className="LandingServicesSection__summary">
+        <LandingServicesSectionSummary summary={summary} />
+      </div>
+      <div className="LandingServicesSection__content">
+        {
+          services.map((service, index) => (
+            <LandingServicesItem
+              key={index}
+              id={service.id}
+              name={service.name}
+              route={service.route}
+              description={service.description}
+            />
+          ))
+        }
+      </div>
+    </div>
+  )
+};
+
+export default LandingServicesSection;
