4be54f4a4e709b9ca2a6d64bef0d9e2c685d49db
William Antony <william@willsquad.com>
2018-12-13 22:17:04 -0600

    Created LandingServicesSectionSummary Component

---
+++ frontend/src/components/fixed/LandingServices/component/LandingServicesSectionSummary/LandingServicesSectionSummary.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './LandingServicesSectionSummary.css';
+import Title from '../../../../reusable/Title/Title';
+
+const LandingServicesSectionSummary = (props) => {
+  const {
+    title,
+    description,
+  } = props.summary;
+
+  const titleStyle = {
+    fontSize: '32px',
+    fontWeight: '700',
+    marginBottom: '20px',
+  };
+
+  return (
+    <React.Fragment>
+      <Title tag="h2" align="left" style={titleStyle}>{ title }</Title>
+      <div className="LandingServicesSectionSummary__description">{ description }</div>
+    </React.Fragment>
+  );
+
+};
+
+export default LandingServicesSectionSummary;
