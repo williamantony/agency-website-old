69694bdc83da2dfde3b246bd5204df6816ffa28b
William Antony <william@willsquad.com>
2018-12-14 23:08:29 -0600

    Created LandingServicesDesign Component

---
+++ frontend/src/components/fixed/LandingServices/LandingServicesDesign/LandingServicesDesign.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import LandingServicesSection from '../component/LandingServicesSection/LandingServicesSection';
+
+const LandingServicesDesign = (props) => {
+  const services = [
+    {
+      id: 'websites',
+      name: 'Websites',
+      route: '/services/website-design',
+      description: 'We create user friendly, secure & high performance websites '+
+        'using the latest in web technologies to help you succeed.',
+    },
+    {
+      id: 'apps',
+      name: 'Applications',
+      route: '/services/application-development',
+      description: 'We develop cross-platform applications that are optimized for user interactions '+
+        'to reduce the gap between you and your audience',
+    },
+  ];
+
+  const summary = {
+    title: 'Design & Development',
+    description: `
+      We are a creative website development company, passionate about helping businesses grow.
+      We take pride in watching the growth of our clients.
+    `,
+  };
+
+  return (
+    <LandingServicesSection summary={summary} services={services} />
+  )
+};
+
+export default LandingServicesDesign;
